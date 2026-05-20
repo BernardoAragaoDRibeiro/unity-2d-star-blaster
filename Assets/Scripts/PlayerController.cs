32be6e5df2f47b71f9e87e8fb18cfad4a32ab42e
Stuffy06138 <bernardoadribeiro03@gmail.com>
2026-05-20 18:04:06 -0300

    Feat: Add player shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float upBoundPadding;
     [SerializeField] float downBoundPadding;
     
+    Shooter playerShooter;
+    
     InputAction moveAction;
+    InputAction fireAction;
     
     Vector3 moveVector;
     Vector2 minBounds;
@@ -18,7 +21,10 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
+        playerShooter = GetComponent<Shooter>();
+        
         moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Fire");
         
         InitBounds();
     }
@@ -26,6 +32,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        FireShooter();
     }
 
     void InitBounds()
@@ -44,5 +51,10 @@ public class PlayerController : MonoBehaviour
         newPos.y = Math.Clamp(newPos.y, minBounds.y + downBoundPadding, maxBounds.y - upBoundPadding);
         
         transform.position = newPos;
+    }
+
+    void FireShooter()
+    {
+        playerShooter.isFiring = fireAction.IsPressed();
     }
 }
