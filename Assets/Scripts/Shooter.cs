77849a1dc430c235da72b39e9a5c9ff500259044
Stuffy06138 <bernardoadribeiro03@gmail.com>
2026-05-21 12:18:20 -0300

    Feat: Add enemy shooting

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,18 +1,31 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("Base Variables")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
-    [SerializeField] float fireRate = 0.2f;
+    [SerializeField] float baseFireRate = 0.2f;
     
-    public bool isFiring;
+    [Header("AI Variables")]
+    [SerializeField] bool useAI;
+    [SerializeField] float minimumFireRate = 0.2f;
+    [SerializeField] float fireRateVariance = 0f;
+    
+    [HideInInspector]public bool isFiring;
     Coroutine fireCoroutine;
 
-    private void Update()
+    private void Start()
+    {
+        if (useAI)
+        {
+            isFiring = true;
+        }
+    }
+
+    void Update()
     {
         Fire();
     }
@@ -36,12 +49,18 @@ public class Shooter : MonoBehaviour
         {
             GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
             
+            projectile.transform.rotation = transform.rotation;
+            
             Rigidbody2D projectileRB = projectile.GetComponent<Rigidbody2D>();
-            projectileRB.linearVelocityY = projectileSpeed;
+            projectileRB.linearVelocity = transform.up * projectileSpeed;
             
             Destroy(projectile, projectileLifetime);
+            
+            float waitTime = UnityEngine.Random.Range(baseFireRate - fireRateVariance, baseFireRate + fireRateVariance);
+            
+            waitTime = Mathf.Clamp(waitTime, minimumFireRate, float.MaxValue);
 
-            yield return new WaitForSeconds(fireRate);
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
